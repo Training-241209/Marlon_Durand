8fbd1d8c2fa35eb3c2db4f10c98e3c144623b734
Marlon Durand <89173825+MarloBarlo@users.noreply.github.com>
2024-12-20 10:04:43 -0600

    Delete ers directory

--- ers/src/main/java/com/revature/ers/repository/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.ers.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.ers.model.Roles;
-
-public interface RolesRepository extends JpaRepository<Roles, Long> {
-
-}
